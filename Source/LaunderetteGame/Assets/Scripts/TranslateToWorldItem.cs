02949d02d66268e1bbd859b5a8f957b70a7d2029
Adam <a.bulman6453@gmail.com>
2024-11-26 10:41:02 +0000

    Made bad code less bad.

--- Source/LaunderetteGame/Assets/Scripts/TranslateToWorldItem.cs
+++ Source/LaunderetteGame/Assets/Scripts/TranslateToWorldItem.cs
@@ -1,26 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class TranslateToWorldItem : MonoBehaviour
 {
     // Editor variables
     [SerializeField]
-    private Transform player;
+    private Transform owner;
     [SerializeField]
-    private CharacterController playerController;
+    private CharacterController ownerController;
     [SerializeField]
     private GameObject[] itemPrefabs;
 
-    // External method
+    // Private stuff
+    private bool ready = true;
+
+    // Methods
+    public void Start()
+    {
+        // Check ready
+        if (owner == null ||
+            ownerController == null ||
+            itemPrefabs == null
+            )
+        {
+            // Tell the script we're not ready
+            ready = false;
+        }
+    }
+    /// <summary>
+    /// Given an ItemID, attempts to generate a worldItem as the owner's root.
+    /// </summary>
+    /// <param name="itemID">The ItemID to generate.</param>
     public void DropItem(int itemID)
     {
-        Instantiate
-        (
-            itemPrefabs[itemID],
-            new Vector3(player.position.x, player.position.y - playerController.height + 0.01f, player.position.z),
-            itemPrefabs[itemID].transform.rotation
-        );
+        // Ensure script is even ready
+        if (ready)
+        {
+            // Ensure itemID is in range
+            if (itemID > itemPrefabs.Length - 1 &&
+                itemID > 0
+                )
+            {
+                // Not in length, so log error and skip
+                Debug.LogWarning($"Failed to translate object to worldItem (Given ID: {itemID}, valid range is IDs: 0 - {itemPrefabs.Length - 1}).");
+                return;
+            }
+
+            // Create a new item matching the ID provided
+            Instantiate
+            (
+                itemPrefabs[itemID],
+                new Vector3(owner.position.x, owner.position.y - ownerController.height + 0.01f, owner.position.z),
+                itemPrefabs[itemID].transform.rotation
+            );
+        }
+        // Otherwise
+        else
+        {
+            // Log error
+            Debug.LogWarning("TranslateToWorldItem does not have all references set-up!");
+        }
     }
 }
