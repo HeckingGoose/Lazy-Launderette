7ef170b1d8d645de5e8c538e4d78738548a3adb9
Adam <a.bulman6453@gmail.com>
2024-11-26 22:59:29 +0000

    Fully moved inventory slot handling to inventory itself. (Mostly)

--- Source/LaunderetteGame/Assets/Scripts/Main/Inventory/InventorySlot.cs
+++ Source/LaunderetteGame/Assets/Scripts/Main/Inventory/InventorySlot.cs
@@ -13,9 +13,72 @@ public class InventorySlot : MonoBehaviour
     private Image _itemImage;
     [SerializeField]
     private TextMeshProUGUI _itemText;
+    [Header("Self Transform")]
+    [SerializeField]
+    private RectTransform _selfTransform;
+    [Header("Animation")]
+    [SerializeField]
+    private float _scaleMultiplier = 1.2f;
+    [SerializeField]
+    private float _rotationAmount = 10;
+    [SerializeField]
+    private float _animationTime = 0.1f;
 
     // Private values
     private Inventory.Item _item = Inventory.Item.None;
+    private bool _selected = false;
+    private float _animationTimer = 0;
+    private Vector3 _baseScale;
+    private Vector3 _baseRotation;
+    private Vector3 _targetScale;
+    private Vector3 _targetRotation;
+
+    // Unity methods
+    private void Start()
+    {
+        // Cache base values
+        _baseScale = transform.localScale;
+        _baseRotation = transform.eulerAngles;
+
+        // Generate target scale
+        _targetScale = _baseScale * _scaleMultiplier;
+
+        // Generate target rotation
+        _targetRotation = new Vector3(
+            _baseRotation.x,
+            _baseRotation.y,
+            _baseRotation.z + _rotationAmount
+            );
+    }
+    private void Update()
+    {
+        // Cache timer
+        float timerCache = _animationTimer;
+
+        // Check whether we are selected
+        switch (_selected)
+        {
+            // We are selected
+            case true:
+                // Increment timer
+                _animationTimer = Mathf.Clamp(_animationTimer + Time.deltaTime, 0, _animationTime);
+                break;
+
+            // We are not selected
+            case false:
+                // Decrement timer
+                _animationTimer = Mathf.Clamp(_animationTimer - Time.deltaTime, 0, _animationTime);
+                break;
+        }
+
+        // If the timer has changed (prevent more maths when not animating)
+        if (timerCache != _animationTimer)
+        {
+            // Do the LERP
+            _selfTransform.localScale = Vector3.Lerp(_baseScale, _targetScale, _animationTimer / _animationTime);
+            _selfTransform.localEulerAngles = Vector3.Lerp(_baseRotation, _targetRotation, _animationTimer / _animationTime);
+        }
+    }
 
     // Methods
     /// <summary>
@@ -33,7 +96,10 @@ public class InventorySlot : MonoBehaviour
         // Set itemID
         _item = item;
     }
-
+    /// <summary>
+    /// Attempts to remove any held item from this slot.
+    /// </summary>
+    /// <returns>A valid inventory item on success, None on fail.</returns>
     public Inventory.Item TryRemoveItem()
     {
         // Cache item
@@ -50,6 +116,20 @@ public class InventorySlot : MonoBehaviour
 
         // Return old item (can be -1 if there never was an item).
         return temp;
+    }
+    /// <summary>
+    /// Tells this slot that it is highlighted.
+    /// </summary>
+    public void Select()
+    {
+        _selected = true;
+    }
+    /// <summary>
+    /// Tells this slot that it is not highlighted.
+    /// </summary>
+    public void DeSelect()
+    {
+        _selected = false;
     }
 
     // Accessors
--- Source/LaunderetteGame/Assets/Scripts/Main/Inventory/ManageInventory.cs
+++ Source/LaunderetteGame/Assets/Scripts/Main/Inventory/ManageInventory.cs
@@ -5,17 +5,9 @@ public class ManageInventory : MonoBehaviour
 {
     // Editor variables
     [SerializeField]
-    private RectTransform[] inventorySlots;
-    [SerializeField]
     private InventorySlot[] _slots;
     [SerializeField]
     private Image heldItemImage;
-    [SerializeField]
-    private float scaleAmount;
-    [SerializeField]
-    private float rotationAmount;
-    [SerializeField]
-    private float maxTimer;
     [SerializeField]
     private TranslateToWorldItem translate;
     [SerializeField]
@@ -24,41 +16,15 @@ public class ManageInventory : MonoBehaviour
     private AudioClip[] dropSounds;
 
     // Private variables
-    private Vector3[] baseScales;
-    private Vector3[] maxScales;
-    private Vector3[] baseRotations;
-    private Vector3[] maxRotations;
     public int currentSlot;
-    private float[] timers;
     private int gState = 0;
 
     private void Start()
     {
-        // Initialise inventory items
-        baseScales = new Vector3[inventorySlots.Length];
-        maxScales = new Vector3[inventorySlots.Length];
-        baseRotations = new Vector3[inventorySlots.Length];
-        maxRotations = new Vector3[inventorySlots.Length];
-        timers = new float[inventorySlots.Length];
-
-        for (int i = 0; i < _slots.Length; i++)
-        {
-            // Setup visual scaling stuff
-            baseScales[i] = inventorySlots[i].localScale;
-            maxScales[i] = baseScales[i] * scaleAmount;
-            baseRotations[i] = inventorySlots[i].localEulerAngles;
-            maxRotations[i] = new Vector3(
-                baseRotations[i].x,
-                baseRotations[i].y,
-                baseRotations[i].z + rotationAmount
-                );
-            timers[i] = 0;
-        }
-
         // Set first item to be a bag
         TryAddItem(Inventory.Item.ClothesBag);
 
-        currentSlot = 0;
+        SelectSlot(0);
     }
 
     private void Update()
@@ -73,46 +39,20 @@ public class ManageInventory : MonoBehaviour
             gState = 0;
         }
 
-        // Iterate through timers
-        for (int i = 0; i < timers.Length; i++)
-        {
-            // Update timer value
-            if (currentSlot == i)
-            {
-                timers[i] += Time.deltaTime;
-                if (timers[i] > maxTimer)
-                {
-                    timers[i] = maxTimer;
-                }
-            }
-            else
-            {
-                timers[i] -= Time.deltaTime;
-                if (timers[i] < 0)
-                {
-                    timers[i] = 0;
-                }
-            }
-
-            // Update scale and rotation of relevant inventory slot
-            inventorySlots[i].localScale = Vector3.Lerp(baseScales[i], maxScales[i], timers[i] / maxTimer);
-            inventorySlots[i].localEulerAngles = Vector3.Lerp(baseRotations[i], maxRotations[i], timers[i] / maxTimer);
-        }
-
         // Poll keys
         if (Input.GetAxis("Inv1") > 0 && currentSlot != 0)
         {
-            currentSlot = 0;
+            SelectSlot(0);
             UpdateHand();
         }
         else if (Input.GetAxis("Inv2") > 0 && currentSlot != 1)
         {
-            currentSlot = 1;
+            SelectSlot(1);
             UpdateHand();
         }
         else if (Input.GetAxis("Inv3") > 0 && currentSlot != 2)
         {
-            currentSlot = 2;
+            SelectSlot(2);
             UpdateHand();
         }
 
@@ -142,6 +82,77 @@ public class ManageInventory : MonoBehaviour
     }
 
     // Other
+    public void IncrementSlot()
+    {
+        // Increment slot
+        currentSlot++;
+
+        // Check for wrap
+        WrapSlot();
+
+        // Select
+        SelectSlot(currentSlot);
+    }
+    public void DecrementSlot()
+    {
+        // Increment slot
+        currentSlot--;
+
+        // Check for wrap
+        WrapSlot();
+
+        // Select
+        SelectSlot(currentSlot);
+    }
+    private void WrapSlot()
+    {
+        // Check lower bound
+        if (currentSlot < 0)
+        {
+            // Wrap
+            currentSlot = _slots.Length - 1;
+            return;
+        }
+
+        // Check upper bound
+        if (currentSlot >= _slots.Length)
+        {
+            // Wrap
+            currentSlot = 0;
+            return;
+        }
+    }
+    private void SelectSlot(int slotIndex)
+    {
+        // Ensure slot index is in range
+        if (slotIndex >= _slots.Length ||
+            slotIndex < 0
+            )
+        {
+            // Early out
+            return;
+        }
+
+        // Loop through all slots
+        for (int i = 0; i < _slots.Length; i++)
+        {
+            // If the slot being selected is this one
+            if (i == slotIndex)
+            {
+                _slots[i].Select();
+            }
+
+            // Otherwise
+            else
+            {
+                // Deselect
+                _slots[i].DeSelect();
+            }
+        }
+
+        // Set slot
+        currentSlot = slotIndex;
+    }
     public void UpdateHand()
     {
         // There is an item in the hand now
