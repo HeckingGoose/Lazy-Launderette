b63ec577401b21e6c36c8920add1da3856d8c477
Adam <a.bulman6453@gmail.com>
2024-11-29 01:20:36 +0000

    Did some builds and changed the launch screen.

--- Source/LaunderetteGame/.utmp/RelWithDebInfo/6r4n493f/armeabi-v7a/cmake_install.cmake
+++ Source/LaunderetteGame/.utmp/RelWithDebInfo/6r4n493f/armeabi-v7a/cmake_install.cmake
@@ -44,6 +44,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
+  include("F:/GithubRepos/Lazy-Launderette/Source/LaunderetteGame/.utmp/RelWithDebInfo/6r4n493f/armeabi-v7a/GameActivity/cmake_install.cmake")
   include("F:/GithubRepos/Lazy-Launderette/Source/LaunderetteGame/.utmp/RelWithDebInfo/6r4n493f/armeabi-v7a/FramePacing/cmake_install.cmake")
 
 endif()
